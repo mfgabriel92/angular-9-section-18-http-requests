3e3730ba77e1d189a3425b2c4f7f731c4e85ed4a
Gabriel M. Fernandes <mfgabriel92@gmail.com>
2020-02-22 09:31:15 -0300

    Backend (Firebase) Setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'section-xviii-http-requests';
+export class AppComponent implements OnInit {
+  posts: Post[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {}
+
+  onCreatePost(data: Post): void {
+    this.http
+      .post('https://angular-9-section-18-http-req.firebaseio.com/', data)
+      .subscribe(response => console.log(response));
+  }
+
+  onFetchPosts(): Post[] {
+    return [];
+  }
+
+  onClearPosts(): void {}
 }
